aaac55da050965880303e4218d12b530bd49bf72
samtenny1609 <93872093+samtenny1609@users.noreply.github.com>
2023-01-12 18:22:20 +0530

    Update fetchApi.js

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-export const baseUrl = 'https://bayut.p.rapidapi.com'
+export const baseUrl = 'https://bayut.p.rapidapi.com/auto-complete'
 
 export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
-      'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': 'iFUdxubHsQmshe51vbzzowIsXzBrp1zQDPGjsnlyuFeYlDeBLs'
+      'X-RapidAPI-Key': '805781675fmshb809fae0a8be11cp17f9ebjsncd5c659356c0',
+      'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
     }
   });
 
