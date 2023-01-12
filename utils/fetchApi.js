a4245070cce0c0403c236216a3c3067304c99318
samtenny1609 <93872093+samtenny1609@users.noreply.github.com>
2023-01-12 18:28:31 +0530

    Update fetchApi.js

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseUrl = 'https://bayut.p.rapidapi.com/auto-complete'
+export const baseUrl = 'https://bayut.p.rapidapi.com'
 
 export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
