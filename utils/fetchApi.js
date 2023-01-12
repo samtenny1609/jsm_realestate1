ea6f48abfb83e8c175f32039fa6772ccd62d35e1
samtenny1609 <93872093+samtenny1609@users.noreply.github.com>
2023-01-12 17:54:53 +0530

    Add files via upload

---
+++ utils/fetchApi.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+
+export const baseUrl = 'https://bayut.p.rapidapi.com'
+/*
+ headers: {
+    'X-RapidAPI-Key': '805781675fmshb809fae0a8be11cp17f9ebjsncd5c659356c0',
+    'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+  }
+*/
+
+export const fetchApi = async(url) => {
+    const {data}= await axios.get((url) , {
+        headers: {
+            'X-RapidAPI-Key': '805781675fmshb809fae0a8be11cp17f9ebjsncd5c659356c0',
+            'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+          }
+    });
+
+    return data;
+}
